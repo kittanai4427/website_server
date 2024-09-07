5c9938b5568b4f877cdcf07e66b48d2d68028fc8
kittanai4427 <58778762+kittanai4427@users.noreply.github.com>
2024-09-07 19:22:08 +0700

    update7/9/2024

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-const next=document.querySelector('#next')
-const prev=document.querySelector('#prev')
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
 
-function handleScrollNext (direction) {
-  const cards = document.querySelector('.card-content')
-  cards.scrollLeft=cards.scrollLeft += window.innerWidth / 2 > 600 ? window.innerWidth /2 : window.innerWidth -100
+
+
+function handleScrollNext() {
+  const cards = document.querySelector('.card-content');
+  const scrollAmount = window.innerWidth > 600 ? window.innerWidth / 2 : window.innerWidth - 100;
+  
+  cards.scrollBy({
+    top: 0,
+    left: scrollAmount,
+    behavior: 'smooth' // เพิ่ม behavior เพื่อให้การเลื่อนไหลลื่น
+  });
 }
 
-function handleScrollPrev (direction) {
-  const cards = document.querySelector('.card-content')
-  cards.scrollLeft=cards.scrollLeft -= window.innerWidth / 2 > 600 ? window.innerWidth /2 : window.innerWidth -100
+function handleScrollPrev() {
+  const cards = document.querySelector('.card-content');
+  const scrollAmount = window.innerWidth > 600 ? window.innerWidth / 2 : window.innerWidth - 100;
+
+  cards.scrollBy({
+    top: 0,
+    left: -scrollAmount,
+    behavior: 'smooth' // เพิ่ม behavior เพื่อให้การเลื่อนไหลลื่น
+  });
 }
 
-next.addEventListener('click', handleScrollNext)
-prev.addEventListener('click', handleScrollPrev)
+next.addEventListener('click', handleScrollNext);
+prev.addEventListener('click', handleScrollPrev);
